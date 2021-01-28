4a6119c119c1c1584eefeb3ae5ddeae3a9f7c934
Syed Ahmed <syedahmed3009@gmail.com>
2021-01-28 12:21:02 +0530

    eact-app

---
+++ build/precache-manifest.679098570ba1509ecda031d0432d3062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b33f59aa3214a4ba29ed",
+    "url": "/static/css/main.c8c8838c.chunk.css"
+  },
+  {
+    "revision": "b33f59aa3214a4ba29ed",
+    "url": "/static/js/main.d213814c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "9426d59aa7ab270cecc19f17db99984c",
+    "url": "/static/media/newLogo.9426d59a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "f9662f174cdab7ea6f7c0f1c7c768661",
+    "url": "/index.html"
+  }
+];
